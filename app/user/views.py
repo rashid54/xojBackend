1e6e0c438850d27abc92cf2df83dd16da85b083e
Rashid <rashid28hasan@gmail.com>
2021-02-07 16:01:48 +0600

    add user list,create,token,manage api

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, authentication, permissions
+from user.serializers import UsersSerializer, AuthTokenSerializer
+from rest_framework.settings import api_settings
+from rest_framework.authtoken.views import ObtainAuthToken
 
-# Create your views here.
+
+class ListUsersView(generics.ListAPIView):
+    """list all exixting users"""
+    serializer_class = UsersSerializer
+    queryset = get_user_model().objects.all()
+
+
+class CreateUsersView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UsersSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage authenticated user model"""
+    serializer_class = UsersSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """get and return authenticated user"""
+        return self.request.user
